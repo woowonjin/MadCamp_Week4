6a3f9276b774424fe4ce583a398698bcdecaa08e
woowonjin <2017029970@hanyang.ac.kr>
2021-01-26 00:05:29 +0900

    GPS Tracking Animation Complete #4

--- MadCamp_Week4/Assets/Scripts/location.cs
+++ MadCamp_Week4/Assets/Scripts/location.cs
@@ -2,28 +2,38 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
-
 public class location : MonoBehaviour
 {
+    static float UPDATE_TIME = 1f;
+    float updateTime = UPDATE_TIME;
     private Rigidbody2D rb;
     bool gpsInit = false;
     LocationInfo currentGPSPosition;
     int gps_connect = 0;
     float detailed_num = 1.0f;//기존 좌표는 float형으로 소수점 자리가 비교적 자세히 출력되는 double을 곱하여 자세한 값을 구합니다.
-    public float cur_latitude;
-    public float cur_longitude;
     public int num_refresh;
-
+    //처음 값
     private float initLat = 36.368169f;
     private float initLng = 127.363846f;
-
     private float initX = -4.9f;
     private float initY = 1.0f;
-
+    //현재 값
+    private float cur_X;
+    private float cur_Y;
+ 
+    //움직일 위치값
+    private float target_X, target_Y;
+    private float target_latitude, target_longitude;
+    
+    //위도, 경도 1당 위치값
     private float perLat = 2851.253867f;
     private float perLng = 2101.14476f;
 
-    private float moveH, moveV;
+    private static float speed = 1.0f;
+
+    //location 임시값
+    private float temp_latitude, temp_longitude;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -32,7 +42,31 @@ public class location : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Input.location.Start(0.5f);
+        Input.location.Start();
+
+        //캐릭터를 기준점으로 이동
+        rb.MovePosition(new Vector2(initX, initY));
+
+        cur_X = initX;
+        cur_Y = initY;
+      
+        target_X = initX;
+        target_Y = initY;
+        
+        target_latitude = initLat;
+        target_longitude = initLng;
+
+        //36.374532, 127.364784
+        //test
+        /*target_latitude = 36.374532f;
+        target_longitude = 127.364784f;
+        gps_connect = 1;
+        target_X = (target_longitude - initLng) * perLng + initX;  //target 좌표
+        target_Y = (target_latitude - initLat) * perLat + initY;   //target 좌표*/
+        //rb.MovePosition(new Vector2(target_X, target_Y));
+
+
+
         int wait = 1000; // 기본 값
         // Checks if the GPS is enabled by the user (-> Allow location )
         if (Input.location.isEnabledByUser)//사용자에 의하여 좌표값을 실행 할 수 있을 경우
@@ -55,23 +89,74 @@ public class location : MonoBehaviour
     }
     void RetrieveGPSData()
     {
-        currentGPSPosition = Input.location.lastData;//gps를 데이터를 받습니다.
-        cur_latitude = currentGPSPosition.latitude * detailed_num; //위도 값을 받아
-        cur_longitude = currentGPSPosition.longitude * detailed_num; //경도 값을 받아
-        gps_connect++;
-        num_refresh = gps_connect;
+        Debug.Log("RetrieveGPSData");
+        if (updateTime > 0)
+        {
+            // nothing
+        }
+        else
+        {
+            if (Input.location.status != LocationServiceStatus.Failed)
+            {
+                currentGPSPosition = Input.location.lastData;//gps를 데이터를 받습니다.
+                cur_X = target_X;
+                cur_Y = target_Y;
+                target_latitude = currentGPSPosition.latitude * detailed_num; //위도 값을 받아
+                target_longitude = currentGPSPosition.longitude * detailed_num; //경도 값을 받아
+                target_X = (target_longitude - initLng) * perLng + initX;  //target 좌표
+                target_Y = (target_latitude - initLat) * perLat + initY;   //target 좌표
+                if (gps_connect == 0)
+                {
+                    rb.MovePosition(new Vector2(target_X, target_Y));
+                    cur_X = target_X;
+                    cur_Y = target_Y;
+                    gps_connect++;
+                }
+                updateTime = UPDATE_TIME;
+            }
+        }
+
     }
+
+        //target_X = (target_longitude - initLng) * perLng + initX;  //target 좌표
+        //target_Y = (target_latitude - initLat) * perLat + initY;   //target 좌표
     void FixedUpdate()
     {
-        moveV = (cur_latitude - initLat) * perLat + initY;
-        moveH = (cur_longitude - initLng) * perLng + initX;
-
-        Debug.Log("moveV : " + moveV + ", moveH : " + moveH);
-        Vector2 temp = new Vector2(moveH, moveV);
-        rb.MovePosition(temp);
-        //rb.velocity = new Vector2(moveH, moveV); // OPTIONAL rb.MovePosition();
-        //rb.position = temp;
-        //Vector2 direction = new Vector2(moveH, moveV);
-        //FindObjectOfType<PlayerAnimation>().SetDirection(direction);
+    }
+    void Update()
+    {
+        //이동해야함
+        Debug.Log("update");
+        updateTime -= Time.deltaTime;
+        if (gps_connect > 0)
+        {
+            Debug.Log("gps connect");
+            Vector2 position = rb.position;
+            if (position.x <= target_X + 0.00001 && position.x >= target_X - 0.00001 && position.y <= target_Y + 0.00001 && position.y >= target_Y - 0.00001)
+            {
+                // Stop
+                Debug.Log("Stop");
+            }
+            else
+            {
+                Debug.Log("target X, Y : " + target_X + ", " + target_Y);
+                Debug.Log("current X, Y : " + cur_X + ", " + cur_Y);
+                float moveX = (target_X - position.x) * Time.deltaTime + position.x;
+                float moveY = (target_Y - position.y) + Time.deltaTime + position.y;
+                Debug.Log("position : " + position);
+                Vector2 move = new Vector2(moveX, moveY);
+                Debug.Log("move" + move);
+                rb.MovePosition(move);
+            }
+        }
+        /*moveY = (cur_latitude - initLat) * perLat + initY;  //target 좌표
+        moveX = (cur_longitude - initLng) * perLng + initX;  //target 좌표
+        Debug.Log("moveY : " + moveY + ", moveX : " + moveX);
+        Vector2 temp = new Vector2(moveX, moveY);
+        rb.MovePosition(temp);*/
+                    //rb.velocity = new Vector2(moveH, moveV); // OPTIONAL rb.MovePosition();
+                    //rb.position = temp;
+                    //Vector2 direction = new Vector2(moveH, moveV);
+                    //FindObjectOfType<PlayerAnimation>().SetDirection(direction);
     }
 }
