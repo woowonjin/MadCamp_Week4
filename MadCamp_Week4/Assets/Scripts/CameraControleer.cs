17daae6a38ee1e94451fc6bb4d5d91082ca3cf64
LJSo0o <53509736+LJSo0o@users.noreply.github.com>
2021-01-25 19:29:18 +0900

    camera complete#3

---
+++ MadCamp_Week4/Assets/Scripts/CameraControleer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControleer : MonoBehaviour
+{
+
+    private Transform target;
+
+    [SerializeField] private float smoothSpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        // Smoothly move Camera
+        // first param : current position
+        // second param : target position
+        // third param : speed
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), smoothSpeed * Time.deltaTime);
+    }
+}
